a8f44436093b9d5b794be30bf0d16822245b0dc5
itsdanjc <dan@itsdanjc.com>
2025-11-19 19:10:58 +0000

    Added sign in button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
 import { Button, Flex, Text, Input, useBreakpointValue  } from "@chakra-ui/react"
-import { BsLayoutSidebar, BsShare } from "react-icons/bs"
+import { BsLayoutSidebar, BsShare, BsBoxArrowInRight } from "react-icons/bs"
 import { useSidebar } from "../App";
 
 export default function Header() {
@@ -23,6 +23,10 @@ export default function Header() {
             <Flex align={"center"} gap={"10px"}>
                 <Button size={"sm"} variant={"outline"}>
                     <BsShare />
+                </Button>
+                <Button size={"sm"} variant={"surface"} colorPalette={"blue"}>
+                    <BsBoxArrowInRight />
+                    <Text>Sign In</Text>
                 </Button>
             </Flex>
         </Flex>
