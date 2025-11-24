809a831de7ffb966ec6df60455c63dc8c7645ba1
itsdanjc <dan@itsdanjc.com>
2025-11-24 15:49:57 +0000

    Added component for sidebar list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
-export default function Sidebar({useMobileVariant = false}){
+import {useState, useEffect} from "react";
+
+export default function Sidebar({useMobileVariant = false}) {
     const CLASS_NAME = "sidebar";
     const CLASS_NAME_MOBILE = `${CLASS_NAME} ${CLASS_NAME}-mobile`;
 
@@ -8,7 +10,30 @@ export default function Sidebar({useMobileVariant = false}){
 
     return (
         <nav className={useMobileVariant ? CLASS_NAME_MOBILE : CLASS_NAME}>
-            <div className={"spinner"}>Loading...</div>
+            <SidebarItemList/>
         </nav>
     )
 };
+
+function SidebarItemList() {
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setItems([]);
+    }, []);
+
+    if (items == null) {
+        // Render loading state
+        return <div>Loading...</div>;
+    }
+    else if (!items || !items.length) {
+        // Render empty state
+        return <div>Empty</div>;
+    }
+
+    // Populated state
+    return (
+        <ul>{items.map(i => <li>{i}</li>)}</ul>
+    );
+}
