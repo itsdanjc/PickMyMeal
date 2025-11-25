a92ac04766201d82b4e34285e700f3f99d8c8baf
itsdanjc <dan@itsdanjc.com>
2025-11-25 19:37:29 +0000

    Added mobile ui variant

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,68 @@
-import { Grid, GridItem } from "@chakra-ui/react"
+import { Grid, GridItem, Drawer, CloseButton, useBreakpointValue } from "@chakra-ui/react"
 import Sidebar from "./components/Sidebar.jsx";
 
-export default function App() {
-  return (
-    <Grid h="100vh" templateRows={"auto 1fr"} templateColumns={"250px 1fr"}>
-        <GridItem colSpan={2} bg="bg.muted">
-            Header
-        </GridItem>
-        <GridItem bg="bg.muted">
-            <Sidebar />
-        </GridItem>
-        <GridItem>
-            Main
-        </GridItem>
-    </Grid>
-  )
+function DesktopLayout() {
+    return (
+        <Grid h="100vh" templateRows={"auto 1fr"} templateColumns={"250px 1fr"}>
+            <GridItem
+                colSpan={2}
+                bg="bg.muted"
+                borderBottomWidth={"1px"}
+                borderColor="colorPalette.800">
+                Header
+            </GridItem>
+            <GridItem
+                bg="bg.muted"
+                borderRightWidth={"1px"}
+                borderColor="colorPalette.800">
+                <Sidebar/>
+            </GridItem>
+            <GridItem>
+                Main
+            </GridItem>
+        </Grid>
+    )
+}
+
+function MobileLayout() {
+    return (
+        <>
+            <Drawer.Root open placement={"start"} size={"xs"}>
+                <Drawer.Backdrop/>
+                <Drawer.Positioner>
+                    <Drawer.Content bg="bg.muted" w={"250px"}>
+                        <Drawer.Header>
+                            <Drawer.Title>
+                                Pick My Meal
+                            </Drawer.Title>
+                        </Drawer.Header>
+                        <Drawer.Body p={0}>
+                            <Sidebar/>
+                        </Drawer.Body>
+                        <Drawer.CloseTrigger asChild>
+                            <CloseButton size="sm"/>
+                        </Drawer.CloseTrigger>
+                    </Drawer.Content>
+                </Drawer.Positioner>
+            </Drawer.Root>
+            <Grid h="100vh" templateRows={"auto 1fr"}>
+                <GridItem
+                    colSpan={2}
+                    bg="bg.muted"
+                    borderBottomWidth={"1px"}
+                    borderColor="colorPalette.800">
+                    Header
+                </GridItem>
+                <GridItem>
+                    Main
+                </GridItem>
+            </Grid>
+        </>
+    )
+}
+
+export default function App(){
+    if (useBreakpointValue({ base: false, md: true }))
+        return <DesktopLayout />;
+    return <MobileLayout />;
 }
